33098e53155cbd57795caa2eed004152d4194486
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-09-15 14:37:13 -0700

    Create Largest Perimeter Triangle.cpp

---
+++ Largest Perimeter Triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int peri = 0;
+        sort(nums.begin(), nums.end());
+        for(int i = nums.size()-1; i>=2; i--){
+            if(nums[i-1]+nums[i-2]> nums[i]){
+                peri = nums[i]+nums[i-1]+nums[i-2];
+                break;
+            }
+        }
+        return peri;
+    }
+};

b0c960a2710e456ae4dce5ce53dc0ee28769b5ae
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-09-17 08:30:55 -0700

    Create Remove Duplicate Letters.cpp

---
+++ Remove Duplicate Letters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int>freq;
+        unordered_map<char, bool>present;
+        stack<char>stack;
+        for(int i=0; i<s.length(); i++){
+            freq[s[i]]++;
+        }
+        for(int i=0; i<s.length(); i++){
+            freq[s[i]]--;
+            if(present[s[i]])
+                continue;
+            while(!stack.empty() && stack.top()>s[i] && freq[stack.top()]>0){
+                present[stack.top()] = false;
+                stack.pop();
+            }
+            stack.push(s[i]);
+            present[s[i]] = true;
+        }
+        string answer = "";
+        while(!stack.empty()){
+            answer = stack.top() + answer;
+            stack.pop();
+        }
+        return answer;
+    }
+};

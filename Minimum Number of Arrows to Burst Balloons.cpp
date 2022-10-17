e364e5d97c8b0992678ee86c9761ee8507be88a3
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-10-17 07:56:49 -0700

    Create Minimum Number of Arrows to Burst Balloons.cpp

---
+++ Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), compare);
+        int arrows = 1, end = points[0][1];
+        for(int i=1; i<points.size(); i++){
+            if(points[i][0]>end){
+                arrows++;
+                end = points[i][1];
+            }
+        }
+        return arrows;
+    }
+    static bool compare(vector<int>&a, vector<int>&b){
+        return a[1]<b[1];
+    }
+};

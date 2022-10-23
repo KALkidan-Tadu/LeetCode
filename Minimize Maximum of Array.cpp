84c722765c3d9e9cb6aa6c54596b50a0dc6afa0d
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-10-23 08:10:26 -0700

    Create Minimize Maximum of Array.cpp

---
+++ Minimize Maximum of Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        long long sum = 0, answer = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            answer = max(answer, ((sum+i)/(i+1)));
+        }
+        return answer;
+    }
+};

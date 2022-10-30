27ae0762241f23b0325576ea9e1a5554c0302613
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-10-30 10:05:04 -0700

    Create DI String Match.cpp

---
+++ DI String Match.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int start=0, end=s.length();
+        vector<int>answer;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='I'){
+                answer.push_back(start);
+                start++;
+            }else{
+                answer.push_back(end);
+                end--;
+            }
+        }
+        answer.push_back(start);
+        return answer;
+    }
+};

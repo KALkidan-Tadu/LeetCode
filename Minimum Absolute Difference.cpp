e7103917de3c2108cedb9205f40acbdbc3ae1d8b
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-11-27 23:26:00 -0800

    Create Minimum Absolute Difference.cpp

---
+++ Minimum Absolute Difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> answer;
+        int minDiff =  INT_MAX;
+        for(int i=1; i<arr.size(); i++)
+            minDiff = min(minDiff, arr[i]-arr[i-1]);
+        for(int i=1; i<arr.size(); i++){
+            if(arr[i]-arr[i-1] == minDiff)
+                answer.push_back({arr[i-1], arr[i]});
+        }
+        return answer;
+    }
+};

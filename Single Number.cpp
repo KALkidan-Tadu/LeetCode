1eeb142a8b5f3bb1df0b27e6f08e00a2480f53f1
KALkidan-Tadu <93763525+KALkidan-Tadu@users.noreply.github.com>
2022-12-12 23:52:34 -0800

    Update Single Number.cpp

--- Single Number.cpp
+++ Single Number.cpp
@@ -1,14 +1,23 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int i=0;
+//         while(i<nums.size()){
+//             if(i<nums.size()-1 && nums[i] == nums[i+1])
+//                 i += 2;
+//             else
+//                 return nums[i];
+//         }
+//         return 0;
+//     }
+// };
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int i=0;
-        while(i<nums.size()){
-            if(i<nums.size()-1 && nums[i] == nums[i+1])
-                i += 2;
-            else
-                return nums[i];
-        }
-        return 0;
+        int answer=0;
+        for(auto n : nums)
+            answer = answer xor n;
+        return answer;
     }
 };
